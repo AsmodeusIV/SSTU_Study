ef86e90665f7b573814f1962e0d5dccf90f0289d
Andrey Kuznetsov <t2uwsmd@gmail.com>
2024-09-18 20:40:07 +0400

    data sciens

--- СГТУ 2024/Grafi/AllCode/AllCode.cpp
+++ СГТУ 2024/Grafi/AllCode/AllCode.cpp
@@ -1,8 +1,27 @@
 ﻿#include <iostream>
-#include "Lesson7.h"
+#include <vector>
+#include "Lesson6.h"
 
-int main()
-{
-    setlocale(LC_ALL, "ru-RU");
-    ex_8();
+using namespace std;
+
+int main() {
+    // Пример списка смежности для дерева
+    vector<vector<int>> edges = { {1, 2}, {2, 5}, {5, 4}, {10, 5}, {6,5}, {6, 3}, {6,9}, {9, 8}, {8, 7}, {8, 11} };
+
+    // Построение кода Прюфера
+    vector<int> code = pruferCode(edges);
+
+    cout << "Prufer Code: ";
+    for (int node : code) {
+        cout << node << " ";
+    }
+    cout << endl;
+
+    /*// Дешифровка кода Прюфера
+    vector<vector<int>> decodedEdges = decodePrufer(code);
+
+    cout << "Decoded Adjacency List:" << endl;
+    printAdjacencyList(decodedEdges);
+    */
+    return 0;
 }
---
+++ СГТУ 2024/Grafi/AllCode/Lesson6.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> pruferCode(const vector<vector<int>>& edges);
